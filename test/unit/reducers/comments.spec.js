d20cfa3c52b15e43a6c4b3a598a697f31580ace7
rodoabad <rodoabad@gmail.com>
2016-02-04 22:15:41 -0600

    Update unit test for comments reducer to test the initial state

--- test/unit/reducers/comments.spec.js
+++ test/unit/reducers/comments.spec.js
@@ -4,4 +4,15 @@ import { comments } from '../../../src/reducers/comments';
 
 describe(`Given the comments reducer`, () => {
 
+  it(`should return the initial state`, () => {
+
+    expect(
+      comments(undefined, {})
+    )
+      .to
+      .deep
+      .equal([]);
+
+  });
+
 });
