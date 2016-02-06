b3cd79345380b2e9e0be2f509f3b15bc6b249863
rodoabad <rodoabad@gmail.com>
2016-02-06 00:53:12 -0600

    Added reducers for adding and deleting comments

--- src/reducers/comments.js
+++ src/reducers/comments.js
@@ -1,6 +1,38 @@
+export function comment(
+  state,
+  action
+) {
+  switch (action.type) {
+    case 'ADD_COMMENT':
+      return {
+        author: action.author,
+        id: action.id,
+        text: action.text
+      };
+    default:
+      return state;
+  }
+}
+
 export function comments(
   state = [],
   action
 ) {
-  return state;
+
+  switch (action.type) {
+    case 'ADD_COMMENT':
+      return [
+        ...state,
+        comment(undefined, action)
+      ];
+    case 'DELETE_COMMENT':
+      return state.filter(
+        comment => {
+          if (comment.id !== action.id) {
+            return comment;
+          }
+        });
+    default:
+      return state;
+  }
 }
--- test/unit/reducers/comments.spec.js
+++ test/unit/reducers/comments.spec.js
@@ -1,32 +1,108 @@
 import { expect } from 'code';
+import deepFreeze from 'deep-freeze';
 
-import { comments } from '../../../src/reducers/comments';
+import { comments as commentsReducer } from '../../../src/reducers/comments';
 
 describe(`Given the comments reducer`, () => {
 
   context(`when passing invalid arguments`, () => {
 
+    let initialState;
+
+    beforeEach(() => {
+
+      initialState = [];
+
+      deepFreeze(initialState);
+
+    });
+
     it(`should return the initial state if you pass an undefined state`, () => {
 
       expect(
-        comments(undefined, {})
+        commentsReducer(undefined, {})
       )
         .to
         .deep
-        .equal([]);
+        .equal(initialState);
 
     });
 
     it(`should return the initial state if you pass an invalid action type`, () => {
 
       expect(
-        comments(undefined, {
+        commentsReducer(undefined, {
           type: 'RANDOM_ACTION_TYPE'
         })
       )
         .to
         .deep
-        .equal([]);
+        .equal(initialState);
+
+    });
+
+  });
+
+  context(`when adding a comment`, () => {
+
+    it(`should add the comment if the right action type is passed`, () => {
+
+      const initialState = [];
+
+      const addCommentAction = {
+        type: 'ADD_COMMENT',
+        author: 'Rodo Abad',
+        id: 0,
+        text: 'This is a comment'
+      };
+
+      const expectedState = [
+        {
+          author: 'Rodo Abad',
+          id: 0,
+          text: 'This is a comment'
+        }
+      ];
+
+      deepFreeze(initialState);
+      deepFreeze(addCommentAction);
+
+      expect(
+        commentsReducer(initialState, addCommentAction)
+      )
+        .to
+        .deep
+        .equal(expectedState);
+
+    });
+
+  });
+
+  context(`when deleting a comment`, () => {
+
+    it(`should delete the comment if the right action is passed`, () => {
+
+      const initialState = [
+        {
+          author: 'Rodo Abad',
+          id: 0,
+          text: 'This is a comment'
+        }
+      ];
+
+      const deleteCommentAction = {
+        type: 'DELETE_COMMENT',
+        id: 0
+      };
+
+      const expectedState = [];
+
+      expect(
+        commentsReducer(initialState, deleteCommentAction)
+      )
+        .to
+        .deep
+        .equal(expectedState);
 
     });
 
