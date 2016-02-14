b31deb597b0a229142dcd9164ed7c5a229ce8647
rodoabad <rodoabad@gmail.com>
2016-02-14 12:12:50 -0600

    Updated `findAllByType` to search for `redux` component containers

--- test/utils/findAllByType.js
+++ test/utils/findAllByType.js
@@ -2,20 +2,20 @@ import React from 'react';
 
 import findAll from './findAll';
 
-export function findAllByType(
-  tree,
-  type
-) {
+export function findAllByType(tree, type) {
 
-  return findAll(tree,
-    component => {
+  return findAll(tree, component => {
 
-      if (React.isValidElement(component)) {
+    if (React.isValidElement(component)) {
 
+      if (component.type.WrappedComponent) {
+        return component.type.WrappedComponent.displayName === type;
+      } else {
         return component.type === type || component.type.displayName === type;
-
       }
 
-    });
+    }
+
+  });
 
 }
