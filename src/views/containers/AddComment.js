93cae4c7894279a691d869893fba9df8dec7330b
rodoabad <rodoabad@gmail.com>
2016-02-17 21:02:05 -0600

    Add the `AddComment` container

---
+++ src/views/containers/AddComment.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { addComment } from '../../actions';
+
+const AddComment = ({ dispatch }) => {
+
+  let author,
+    text;
+
+  return (
+    <div>
+      {'Name: '}
+      <input
+        className="author"
+        ref={node => author = node}
+        type="text"
+      />
+      <br/>
+      {'Comment: '}
+      <input
+        className="text"
+        ref={node => text = node}
+        type="text"
+      />
+      <br/>
+      <button
+        onClick={() => {
+          dispatch(addComment({
+            author: author.value,
+            text: text.value
+          }));
+        }}
+      >
+        {'Add Comment'}
+      </button>
+    </div>
+  );
+};
+
+AddComment.displayName = 'AddComment';
+
+export default connect()(AddComment);
---
+++ test/unit/containers/AddComment.spec.js
@@ -0,0 +1,74 @@
+import { expect } from 'code';
+import Chance from 'chance';
+import deepFreeze from 'deep-freeze';
+
+import * as TestUtils from '../../utils';
+import AddComment from '../../../src/views/containers/AddComment';
+
+describe('Given the App component', () => {
+
+  let addCommentEl;
+
+  beforeEach('Setup', () => {
+
+    addCommentEl = TestUtils.shallowRenderElement(AddComment.WrappedComponent);
+
+  });
+
+  it('should have a div', () => {
+
+    expect(addCommentEl.type).equal('div');
+
+  });
+
+  describe('given the author input', () => {
+
+    let inputAuthorEl;
+
+    beforeEach(() => {
+
+      inputAuthorEl = TestUtils.findByClassName(addCommentEl, 'author');
+
+    });
+
+    it('should be a text input', () => {
+
+      expect(inputAuthorEl.type).equal('input');
+      expect(inputAuthorEl.props.type).equal('text');
+
+    });
+
+    it('should have a className', () => {
+
+      expect(inputAuthorEl.props.className).equal('author');
+
+    });
+
+  });
+
+  describe('given the comment input', () => {
+
+    let inputCommentEl;
+
+    beforeEach(() => {
+
+      inputCommentEl = TestUtils.findByClassName(addCommentEl, 'text');
+
+    });
+
+    it('should be a text input', () => {
+
+      expect(inputCommentEl.type).equal('input');
+      expect(inputCommentEl.props.type).equal('text');
+
+    });
+
+    it('should have a className', () => {
+
+      expect(inputCommentEl.props.className).equal('text');
+
+    });
+
+  });
+
+});
